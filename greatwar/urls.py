e6e384ea9f4b780adc87512a83c0a67840f79911
rlskoeser <rebecca.s.koeser@emory.edu>
2015-06-15 16:35:17 -0400

    Fix relative view names in urls

--- greatwar/urls.py
+++ greatwar/urls.py
@@ -10,10 +10,10 @@ from django.conf.urls.static import static
 urlpatterns = patterns('',
     # Example:
     # (r'^greatwar/', include('greatwar.foo.urls')),
-    url(r'^$', 'views.index', name="index"),
-    url(r'^about/$', 'views.about', name="about"),
-    url(r'^links/$', 'views.links', name="links"),
-    url(r'^credits/$', 'views.credits', name="credits"),
+    url(r'^$', 'greatwar.views.index', name="index"),
+    url(r'^about/$', 'greatwar.views.about', name="about"),
+    url(r'^links/$', 'greatwar.views.links', name="links"),
+    url(r'^credits/$', 'greatwar.views.credits', name="credits"),
     url(r'^poetry/', include('greatwar.poetry.urls', namespace='poetry')),
     url(r'^postcards/', include('greatwar.postcards.urls', namespace='postcards')),
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
@@ -28,4 +28,5 @@ urlpatterns = patterns('',
 if settings.DEV_ENV:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
