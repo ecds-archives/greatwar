5b4862bd48aaed6667df348b7ee7a93abe3f02f5
rlskoeser <rebecca.s.koeser@emory.edu>
2016-07-27 16:30:42 -0400

    Display postcard images with IIIF image server instead of fedora/djatoka

--- greatwar/postcards/models.py
+++ greatwar/postcards/models.py
@@ -9,7 +9,7 @@ from eulexistdb.models import XmlModel
 
 from eulfedora.server import Repository
 from eulfedora.models import DigitalObject, FileDatastream, XmlDatastream
-
+from piffle.iiif import IIIFImageClient
 
 from pidservices.djangowrapper.shortcuts import DjangoPidmanRestClient
 from util import get_pid_target
@@ -35,7 +35,7 @@ class KeyValue(XmlModel, TeiInterp):
 
 # preliminary fedora object for images
 class ImageObject(DigitalObject):
-    CONTENT_MODELS = [ 'info:fedora/emory-control:Image-1.0' ]
+    CONTENT_MODELS = ['info:fedora/emory-control:Image-1.0']
     IMAGE_SERVICE = 'emory-control:DjatokaImageService'
 
     # DC & RELS-EXT inherited
@@ -46,17 +46,23 @@ class ImageObject(DigitalObject):
 
     default_pidspace = getattr(settings, 'FEDORA_PIDSPACE', None)
 
-    def thumbnail(self):
-        'Shortcut to thumbnail-size image dissemination.'
-        return self.getDissemination(self.IMAGE_SERVICE, 'getRegion', {'level': '1'})
+    @property
+    def iiif_image(self):
+        img_id = '%s%s' % (getattr(settings, 'IIIF_ID_PREFIX', ''),
+                           self.pid)
+        return IIIFImageClient(settings.IIIF_API_ENDPOINT, img_id)
 
-    def medium_image(self):
-        'Shortcut to medium-size image dissemination.'
-        return self.getDissemination(self.IMAGE_SERVICE, 'getRegion', {'level': '3'})
+    @property
+    def thumbnail_url(self):
+        return self.iiif_image.size(width=122, height=122, exact=True).format('png')
 
-    def large_image(self):
-        'Shortcut to large-size image dissemination.'
-        return self.getDissemination(self.IMAGE_SERVICE, 'getRegion', {'level': '4'})
+    @property
+    def medium_img_url(self):
+        return self.iiif_image.size(width=400, height=400, exact=True).format('png')
+
+    @property
+    def large_img_url(self):
+        return self.iiif_image.size(width=800, height=800, exact=True).format('png')
 
     def get_default_pid(self):
         # try to configure a pidman client to get pids.
@@ -71,7 +77,6 @@ class ImageObject(DigitalObject):
         pid = '%s:%s' % (self.default_pidspace, noid)
         self.dc.content.identifier_list.append(ark) # Store local identifiers in DC
         return pid
-
 
 # map interpgroup into a categories object that can be used as fedora datastream class
 class RepoCategories(_TeiBase):
